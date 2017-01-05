6cc2ea6d043c5fa6f181cfe14763488593d99119
brharr <brharr@microsoft.com>
2017-01-05 11:53:54 -0500

    New Commits

---
+++ Azure Backup/BackupVault&PolicyDeployment.ps1
@@ -0,0 +1,8 @@
+﻿Login-AzureRmAccount
+
+$location = "South Central US"
+$rgName = "GettingStarted-Backup"
+
+New-AzureRmResourceGroup -Location $location -Name $rgName -Verbose
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rgName -TemplateFile "azuredeploy.json" -TemplateParameterFile "azuredeploy.parameters.json" -Name "BackupDeployment" -Mode Incremental -Verbose
