4a6c176daad530c1f14b4665cb98eeeda4d480f8
brharr <brharr@microsoft.com>
2017-01-13 18:07:58 -0500

    Enabling Protection

---
+++ Azure Backup/Enable VM Protection.ps1
@@ -0,0 +1,8 @@
+﻿Login-AzureRmAccount
+
+$rgName = "GettingStarted-Backup"
+
+$vault = Get-AzureRmRecoveryServicesVault -Name "GettingStarted-BackupVault" | Set-AzureRmRecoveryServicesVaultContext
+$pol = Get-AzureRmRecoveryServicesBackupProtectionPolicy -Name "DefaultWeeklyPolicy"
+
+Get-AzureRmVM -ResourceGroupName $rgName | ForEach-Object {Enable-AzureRmRecoveryServicesBackupProtection -Policy $pol -ResourceGroupName $rgName -Name $_.Name}
